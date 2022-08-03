4f9795c9a80c7824e3a76ece1e3940e54dc085db
AslihanAkbulutt <82841457+AslihanAkbulutt@users.noreply.github.com>
2022-08-03 19:18:45 +0300

    Update Liste.hpp

--- 2.Proje/include/Liste.hpp
+++ 2.Proje/include/Liste.hpp
@@ -1,11 +1,4 @@
-/**
-* @file        Liste.hpp
-* @description Liste sınıfının değişken ve fonksiyonlarını tanımladım.
-* @course      Bilgisayar Mühendisliği 2.sınıf veri yapıları dersi 2B 
-* @assignment  1.Ödev
-* @date        5.11.2021
-* @author      Aslıhan Akbulut aslihan.akbulut1@ogr.sakarya.edu.tr
-*/
+
 #ifndef LISTE
 #define LISTE
 #include<string>

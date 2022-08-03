5c438d36f808580c2a22f9b044955a6fe5ee87f0
AslihanAkbulutt <82841457+AslihanAkbulutt@users.noreply.github.com>
2022-08-03 19:18:24 +0300

    Update Dugum.hpp

--- 2.Proje/include/Dugum.hpp
+++ 2.Proje/include/Dugum.hpp
@@ -1,11 +1,3 @@
-/**
-* @file        Dugum.hpp
-* @description Dugum sınıfının değişken ve fonksiyonlarını tanımladım.
-* @course      Bilgisayar Mühendisliği 2.sınıf veri yapıları dersi 2B 
-* @assignment  1.Ödev
-* @date        5.11.2021
-* @author      Aslıhan Akbulut aslihan.akbulut1@ogr.sakarya.edu.tr
-*/
 
 #ifndef Dugum_hpp
 #define Dugum_hpp
